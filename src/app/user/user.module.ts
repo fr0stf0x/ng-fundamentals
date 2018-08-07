03a38bc643541ae0c73a163e489a7f8d0e13f137
Fr0stF0x <tang.xuanthao1996@gmail.com>
2018-08-07 17:43:31 +0700

    a

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile.component';
+import { FormsModule } from '@angular/forms';
 
 import { userRoutes } from './user.routes';
 import { LoginComponent } from './login.component';
@@ -9,6 +10,7 @@ import { LoginComponent } from './login.component';
 
 @NgModule({
     imports: [
+        FormsModule,
         CommonModule,
         RouterModule.forChild(userRoutes)
     ],
